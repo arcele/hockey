b6543b48a970c7c6db4d7f19080019a94f2319b1
J. Ryan Kelley <ryan.kelley@espn.com>
2014-01-02 23:39:28 -0800

    fixing formatting

--- js/nwbhf.js
+++ js/nwbhf.js
@@ -1,23 +1,22 @@
 (function () {
      // Commenting out proper scoping for console access     var Hockey = {};
-     Hockey = {};
-     Hockey.stage = new Kinetic.Stage(
-	 { container: 'container',
-	   width:800,
-	   height:800
-	 }
-     );
+    Hockey = {};
+    Hockey.stage = new Kinetic.Stage({
+        container: 'container',
+        width:800,
+        height:800
+	});
 
-     Hockey.rink = new Rink(Hockey.stage);
-     Hockey.puck = new Puck(Hockey);
+    Hockey.rink = new Rink(Hockey.stage);
+    Hockey.puck = new Puck(Hockey);
 
-     Hockey.teams = new Array();
+    Hockey.teams = new Array();
 
-     var playersLayer = new Kinetic.Layer( {id: 'players'} );
-     for(var i = 1; i <= 2; ++i) {
-        Hockey.teams.push(new Team(Hockey, i, playersLayer));
-     }
+    var playersLayer = new Kinetic.Layer( {id: 'players'} );
+    for(var i = 1; i <= 2; ++i) {
+    Hockey.teams.push(new Team(Hockey, i, playersLayer));
+    }
 
-     Hockey.stage.add(playersLayer);
-     
+    Hockey.stage.add(playersLayer);
+
 })();
